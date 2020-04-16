a5cf9baf64c4aa737386ec1e8710b518a397449a
krasnovss91 <krasnovstanislav91@gmail.com>
2020-04-16 15:54:26 +0400

    first commit

---
+++ UserServlet+DB/src/UserServlet.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/users")
+public class UserServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        try{
+            String url = "jdbc:mysql://localhost/users?serverTimezone=Europe/Moscow&useSSL=false";
+            String username = "root";
+            String password = "password";
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            try (Connection conn = DriverManager.getConnection(url, username, password)){
+
+                writer.println("Connection to UserDB succesfull!");
+            }
+        }
+        catch(Exception ex){
+            writer.println("Connection failed...");
+            writer.println(ex);
+        }
+        finally {
+            writer.close();
+        }
+    }
+}
